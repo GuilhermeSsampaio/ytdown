228e58b5ad8be4c6033bb078c897cf37baaeb396
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2026-09-18 21:22:00 -0400

    update build

--- build_exe.bat
+++ build_exe.bat
@@ -2,7 +2,7 @@
 setlocal
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-python -m PyInstaller --noconfirm --clean --onefile --name YtDown --add-data "templates;templates" --add-data "static;static" --collect-all imageio_ffmpeg app.py
+python -m PyInstaller --noconfirm --clean --onefile --name YtDown --add-data "templates;templates" --add-data "static;static" --collect-all imageio_ffmpeg --collect-all pytubefix app.py
 echo.
 echo Pronto: dist\YtDown.exe
 pause
